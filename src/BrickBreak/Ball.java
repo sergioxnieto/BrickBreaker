b4658fb88cf806952f0a113b3d653a5dd87a02c6
Sergio Nieto <s.nieto.t@gmail.com>
2020-06-08 20:03:42 -0700

    Checkpoint for Ball/Brick Classes

--- src/BrickBreak/Ball.java
+++ src/BrickBreak/Ball.java
@@ -1,5 +1,20 @@
 package BrickBreak;
 
-public class Ball {
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
+public class Ball implements Entity {
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Rectangle generateHitbox() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
--- src/BrickBreak/Brick.java
+++ src/BrickBreak/Brick.java
@@ -1,5 +1,20 @@
 package BrickBreak;
 
-public class Brick {
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Brick implements Entity {
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Rectangle generateHitbox() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
