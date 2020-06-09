0843eb8dc09ddd7a5e8fe82687385ae44c65ebe5
Sergio Nieto <s.nieto.t@gmail.com>
2020-06-08 20:03:09 -0700

    Updated Game to Display Player
    * Finished up modifications to the player class
    * Game can display the player and update according to key presses

--- src/BrickBreak/Entity.java
+++ src/BrickBreak/Entity.java
@@ -1,9 +1,11 @@
 package BrickBreak;
 
-public interface Entity {
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
+public interface Entity {
 	
-	
-	
+	public void paint(Graphics g);
+	public Rectangle generateHitbox();
 	
 }
--- src/BrickBreak/Game.java
+++ src/BrickBreak/Game.java
@@ -1,26 +1,43 @@
 package BrickBreak;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import javax.swing.Timer;
 import javax.swing.JPanel;
 
 public class Game extends JPanel implements KeyListener, ActionListener {
 
+	private boolean play = false;
+	private int score = 0;
+	private Timer timer;
+	private int delay = 8;
+	
 	// Constructor
 	public Game() {
-		
-		
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		timer = new Timer(delay, this);
+		timer.start();
 	}
 	
 	
+	// Intermediate instantiations until factory is setup
+	Paddle player = new Paddle();
 	
 	// Handles drawing features to the game window
 	public void paint(Graphics g) {
-	
+		// background
+		g.setColor(Color.black);
+		g.fillRect(0, 0, 700, 600);
+		
+		// paddle
+		player.paint(g);
 
 		g.dispose();
 	}
@@ -31,10 +48,33 @@ public class Game extends JPanel implements KeyListener, ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
+		
+		// Update the canvas
+		repaint();
 	}
+	
 	// keyPressed handles key detection
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			if(player.getX() >= 600) {
+				player.setX(600);
+			}
+			else {
+				play = true;
+				player.moveRight();
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			if(player.getX() <= 0) {
+				player.setX(0);
+			}
+			else {
+				play = true;
+				player.moveLeft();
+			}
+		}
+		
+		// Eventually setup a key to restart
 		
 	}
 	// Functions must be implemented due to interface
